5c703990a0fcbcaedf832615780dc80ad1e966c3
mariioogg <mgregorio@edu.tecnocampus.cat>
2023-04-19 13:26:47 +0200

    Commit11

--- src/test/java/com/tecnocampus/ES3/BowlingGameTest.java
+++ src/test/java/com/tecnocampus/ES3/BowlingGameTest.java
@@ -8,12 +8,21 @@ public class BowlingGameTest {
     private Game g;
     @BeforeEach
     public void setUp(){
+
         g = new Game();
     }
 
     @Test
     public void rollABall(){
         g.roll(0);
+    }
+
+    @Test
+    public void gutter_game(){
+        for(var i = 0; i < 20; i += 1)
+            g.roll(0);
+
+        assertThat(g.score()).isEqualTo(0);
     }
 
 }
