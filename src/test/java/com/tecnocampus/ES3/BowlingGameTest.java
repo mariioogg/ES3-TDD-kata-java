22e1dd1af1a21c059aef12d2d91213428d496022
mariioogg <mgregorio@edu.tecnocampus.cat>
2023-04-19 13:28:40 +0200

    Commit13

--- src/test/java/com/tecnocampus/ES3/BowlingGameTest.java
+++ src/test/java/com/tecnocampus/ES3/BowlingGameTest.java
@@ -15,11 +15,6 @@ public class BowlingGameTest {
     }
 
     @Test
-    public void rollABall(){
-        g.roll(0);
-    }
-
-    @Test
     public void gutter_game(){
         for(var i = 0; i < 20; i += 1)
             g.roll(0);
